5097d5658eddd1beefc0c4141ad983163aaeb8f9
JMespoir <maro3534@knu.ac.kr>
2025-05-31 22:55:29 +0900

    fix error

--- frontend/lib/axiosInstance.ts
+++ frontend/lib/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.219.61:5000/api', // 📌 실제 실행환경에서는 로컬 IP로 교체 필요
+  baseURL: `http://localhost:5000/api`, // 📌 실제 실행환경에서는 로컬 IP로 교체 필요
   headers: {
     'Content-Type': 'application/json',
   },
